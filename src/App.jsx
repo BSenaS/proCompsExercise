56bcc2734ef3bda63801282a5b69cfd8c0ea2564
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-06-13 20:40:01 +0300

    Dummy Data Created for simulating fetching data from api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,4 @@
 import "./App.css";
-import filterPick from "./assets/GameStyleFilters/filterPick.svg";
-import filterSiege from "./assets/GameStyleFilters/filterSiege.svg";
-import filterEngage from "./assets/GameStyleFilters/filterEngage.svg";
-import filterProtect from "./assets/GameStyleFilters/filterProtect.svg";
-import filterSplit from "./assets/GameStyleFilters/filterSplit.svg";
-
-import scalingEarly from "./assets/ScalingVariants/scalingEarly.svg";
-import scalingMid from "./assets/ScalingVariants/scalingMid.svg";
-import scalingMid2 from "./assets/ScalingVariants/scalingMid2.svg";
-import scalingLate from "./assets/ScalingVariants/scalingLate.svg";
-import scalingAllAround from "./assets/ScalingVariants/scalingAllAround.svg";
-
-import engageIcon from "./assets/GameStyleIcons/engageIcon.svg";
-
-import teamIcon from "./assets/teamIcon.svg";
 
 function App() {
   return (
---
+++ src/data.js
@@ -0,0 +1,168 @@
+//Api simulation
+
+//Champions Assets
+import Ahri from "../public/champions/Ahri.png";
+import Akali from "../public/champions/Akali.png";
+import Ashe from "../public/champions/Ashe.png";
+import Brand from "../public/champions/Brand.png";
+import Briar from "../public/champions/Briar.png";
+
+//GameStyles assets.
+import engageIcon from "./assets/GameStyleIcons/engageIcon.svg";
+import pickIcon from "./assets/GameStyleIcons/pickIcon.svg";
+import protectIcon from "./assets/GameStyleIcons/protectIcon.svg";
+import siegeIcon from "./assets/GameStyleIcons/siegeIcon.svg";
+import splitIcon from "./assets/GameStyleIcons/splitIcon.svg";
+
+//Comp Scaling Assets
+import scalingAllAround from "./assets/ScalingVariants/scalingAllAround.svg";
+import scalingEarly from "./assets/ScalingVariants/scalingEarly.svg";
+import scalingLate from "./assets/ScalingVariants/scalingLate.svg";
+import scalingMid from "./assets/ScalingVariants/scalingMid.svg";
+
+export const gameData = [
+  {
+    compName: "Protech Ashe (Late)",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Protect",
+        icon: protectIcon,
+      },
+    ],
+    scale: scalingLate,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "Crust the enemy (Early)",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingEarly,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "Favorite comp for scaling in to late game (Late)",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Siege",
+        icon: siegeIcon,
+      },
+    ],
+    scale: scalingLate,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example 1",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Pick",
+        icon: pickIcon,
+      },
+    ],
+    scale: scalingMid,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example 2",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Split",
+        icon: splitIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example Win",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example Win",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example Win",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example Win",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example Win",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example Win",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+  {
+    compName: "CompName Example Win",
+    champions: [Ahri, Akali, Ashe, Brand, Briar],
+    gameStyles: [
+      {
+        name: "Engage",
+        icon: engageIcon,
+      },
+    ],
+    scale: scalingAllAround,
+    createTime: "8 hours ago",
+  },
+];
